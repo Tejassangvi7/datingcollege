336ee5e61555fe86592175d6fe349c37f45ce335
Tejassangvi7 <sangvitejas7@gmail.com>
2025-08-01 02:10:25 +0530

    Update EnterOTP.jsx

--- src/pages/authorized/EnterOTP.jsx
+++ src/pages/authorized/EnterOTP.jsx
@@ -9,7 +9,7 @@ const EnterOTP = ({ email, onNext }) => {
   const handleVerify = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("https://collegedating-1.onrender.com/api/v1/auth/verify-otp-1st", { email, otp });
+      await axios.post("https://collegedating-cbii.onrender.com/api/v1/auth/verify-otp-1st", { email, otp });
       toast.success("OTP Verified!");
       onNext();
     } catch (err) {
